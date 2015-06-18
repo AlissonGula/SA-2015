f6c025aaee6ae2c8ad8f5ef26873da6258ca77ea
alisson_augusto <alisson_augusto@SNFNS2EP14VM082.florianopolis.sc.senai.br>
2015-06-17 21:51:27 -0300

    Login imclompleto -- Cardapio

---
+++ src/senai/sc/br/situacao2015/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+package senai.sc.br.situacao2015.dao;
+
+import javax.persistence.Query;
+
+import senai.sc.br.situacao2015.entity.Usuario;
+
+
+
+public class UsuarioDao extends Dao {
+	
+	public Usuario buscaPonNome(String nome) {
+		Query query = getEntityManager().createQuery("From Usuario u Where u.nome = :nome", Usuario.class);
+		query.setParameter("nome", nome);
+		return (Usuario) query.getSingleResult();
+	}
+}
